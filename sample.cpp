1b9fe43cbb22debaf87c3055fde12cade576097a
SHIRAKURA Tetsuya <shirakura@wombat-or.jp>
2021-06-12 20:30:27 +0900

    PHPを追加

--- sample.cpp
+++ sample.cpp
@@ -3,32 +3,42 @@
 
 void f1(std::array<int, 3> c)
 {
-    c[0] = 4;
+    c[0] = 5;
 }
 
 void f2(std::array<int, 3>& c)
 {
-    c[0] = 4;
+    c[0] = 5;
 }
 
 int main(int argc, char* argv[])
 {
-    std::array<int, 3> a { 1, 2, 3 };
-    auto b = a;
-    b[0] = 4;
-    for (int i : a) {
+    std::array<int, 3> a1 { 1, 2, 3 };
+    auto b1 = a1;
+    b1[0] = 4;
+    for (int i : a1) {
         std::cout << i << " ";
     }
     std::cout << std::endl;
 
-    f1(a);
-    for (int i : a) {
+    std::array<int, 3> a2 { 1, 2, 3 };
+    auto& b2 = a2;
+    b2[0] = 4;
+    for (int i : a2) {
         std::cout << i << " ";
     }
     std::cout << std::endl;
 
-    f2(a);
-    for (int i : a) {
+    std::array<int, 3> a3 { 1, 2, 3 };
+    f1(a3);
+    for (int i : a3) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::array<int, 3> a4 { 1, 2, 3 };
+    f2(a4);
+    for (int i : a4) {
         std::cout << i << " ";
     }
     std::cout << std::endl;
